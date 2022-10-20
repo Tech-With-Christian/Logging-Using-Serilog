f050b359d78ea7aef5fad208319af20c26c4d5be
Christian Schou <christian.schou@hotmail.com>
2022-10-20 23:47:40 +0200

    Extend logging programmatically

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
 ﻿using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 Log.Logger = new LoggerConfiguration()
 
-                            // Add console (Sink) as logging target
-                            .WriteTo.Console()
+    // Add console (Sink) as logging target
+    .WriteTo.Console()
 
-                            // Set default minimum log level
-                            .MinimumLevel.Debug()
+    // Write logs to a file for warning and logs with a higher severity
+    // Logs are written in JSON
+    .WriteTo.File(new JsonFormatter(),
+        "important-logs.json",
+        restrictedToMinimumLevel: LogEventLevel.Warning)
 
-                            // Create the actual logger
-                            .CreateLogger();
+    // Add a log file that will be replaced by a new log file each day
+    .WriteTo.File("all-daily-.logs",
+        rollingInterval: RollingInterval.Day)
 
-// See https://aka.ms/new-console-template for more information
+    // Set default minimum log level
+    .MinimumLevel.Debug()
+
+    // Create the actual logger
+    .CreateLogger();
+
 Console.WriteLine("Hello, World!");
 
 Log.CloseAndFlush();
