71e52820f12570b7ede59b20c6bc9de4c840ffc7
Christian Schou <christian.schou@hotmail.com>
2022-10-20 23:34:46 +0200

    Add Default Serilog Logging Configuration
    Added default Serilog Logging Configuration using the programmatic way

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+
+                            // Add console (Sink) as logging target
+                            .WriteTo.Console()
+
+                            // Set default minimum log level
+                            .MinimumLevel.Debug()
+
+                            // Create the actual logger
+                            .CreateLogger();
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Log.CloseAndFlush();
 
